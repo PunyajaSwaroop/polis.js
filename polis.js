d29a5022a1ff9b21862b4d57eb66bf239b5667f9
PunyajaSwaroop <punyaja@yahoo.com>
2016-09-22 12:32:32 +0530

    Update polis.js

--- polis.js
+++ polis.js
@@ -692,11 +692,7 @@ function pgmDetails() {
 }
 function chatInitiate() {
     return ({
-        speech: "Hello, I am Verizon Entertainment bot.
-        
-        
-        I can help you with  TV Recommendations or Recording a program. 
-        What would you like to do?",
+        speech: "Hello, I am Verizon Entertainment bot. I can help you with  TV Recommendations or Recording a program.What would you like to do?",
        
         displayText: "TV Recommendations",
         data: {
@@ -705,13 +701,7 @@ function chatInitiate() {
                     "type": "template",
                     "payload": {
                         "template_type": "button",
-                        "text": "Hi, I am Verizon Entertainment bot.  
-                        
-                        
-                        I can help you with  TV Recommendations or Recording a program. 
-                        
-                        
-                        What would you like to do?",
+                        "text": "Hi, I am Verizon Entertainment bot. I can help you with  TV Recommendations or Recording a program.  What would you like to do?",
                         "buttons": [
                             {
                                 "type": "postback",
